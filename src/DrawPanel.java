c4e62ab490faaa8067255523b3ae748c79719881
James Margolis <margolisjames151@gmail.com>
2025-10-19 22:30:31 +0300

    Added DrawPanel for output graphic information on screen

---
+++ src/DrawPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class DrawPanel extends JPanel {
+    public DrawPanel() {
+
+    }
+
+    @Override
+    public boolean isPaintingTile() {
+        return super.isPaintingTile();
+    }
+}
